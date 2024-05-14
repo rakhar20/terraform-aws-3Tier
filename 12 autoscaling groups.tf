7230f0fe9c50df3be68ca773cde37d028e61a583
rakhar20 <prakharjoshi25@gmail.com>
2024-05-14 14:15:39 +0530

    5

--- 12 autoscaling groups.tf
+++ 12 autoscaling groups.tf
@@ -4,8 +4,7 @@ resource "aws_launch_template" "auto-scaling-group" {
     name_prefix = "autoscaling-group"
     image_id = data.aws_ami.ami.id
     instance_type = var.ins_type
-    user_data = file("${path.module}/install-apache.sh"))
-    key_name = "source_key"
+    user_data = file("${path.module}/install-apache.sh")
     network_interfaces {
         subnet_id = aws_subnet.public-web-subnet-1.id
         security_groups = [aws_security_group.web-sg.id]
