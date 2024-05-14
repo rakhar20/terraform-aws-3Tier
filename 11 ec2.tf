eeb96edc4a20226fb651417c6a3514519c3ab950
rakhar20 <prakharjoshi25@gmail.com>
2024-05-14 14:04:54 +0530

    5

--- 11 ec2.tf
+++ 11 ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "publicweb" {
     user_data = <<-EOF
         #!?bin/bash
         yum update-y
-        yun installl -y httpd
+        yum installl -y httpd
         systemstl start httpd
         systemctl enable https 
         echo "<html><body><h1>Web tier"</h1></body></html>" > /var/www/html/index.html
